387f78fac6695fa328d025e4c65cf5abac7613d7
Prabesh Baskota <80308797+s333608@users.noreply.github.com>
2026-02-07 21:47:37 +0930

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This tells Vite where your site is hosted on GitHub Pages
+  base: '/YOUR_REPO_NAME/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
